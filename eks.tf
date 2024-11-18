aff4b09789a994dd9134a1cad40197b62524d049
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-11-17 20:01:30 -0700

    quick public test

--- eks.tf
+++ eks.tf
@@ -86,9 +86,9 @@ module "eks" {
     }
   }
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = false
-  public_access_cidrs = ["0.0.0.0/0"]
+  cluster_public_access_cidrs     = ["0.0.0.0/0"]
 
   tags = {
     Environment = var.environment
