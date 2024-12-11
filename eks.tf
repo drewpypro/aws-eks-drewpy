b3534a7c2c94cd728704de17362bbb1e5a9bbbdd
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-12-10 19:33:15 -0700

    updated iam

--- eks.tf
+++ eks.tf
@@ -1,3 +1,10 @@
+module "security_groups" {
+  source  = "git::https://github.com/drewpypro/terraform-aws-sg-module-template.git?ref=v2.0.0"
+
+  vpc_id = module.vpc.vpc_id
+
+}
+
 resource "aws_eks_cluster" "eks" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
