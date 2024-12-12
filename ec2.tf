7711056f09aaa7bbecdd99ce946420de0e42b2ba
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-12-12 11:22:04 -0700

    remove eip and add new sg

--- ec2.tf
+++ ec2.tf
@@ -29,12 +29,12 @@ resource "aws_instance" "test_ec2" {
   ]
 }
 
-## ELASTIC IP
-resource "aws_eip" "test_ec2_eip" {
-  instance = aws_instance.test_ec2.id
-  domain   = "vpc"
-}
+# ## ELASTIC IP
+# resource "aws_eip" "test_ec2_eip" {
+#   instance = aws_instance.test_ec2.id
+#   domain   = "vpc"
+# }
 
-output "test_ec2_public_ip" {
-  value = aws_eip.test_ec2_eip.public_ip
-}
+# output "test_ec2_public_ip" {
+#   value = aws_eip.test_ec2_eip.public_ip
+# }
