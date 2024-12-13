9e52aef3717c66762c91a073f4ef3fa3f3475bb0
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-12-13 12:00:40 -0700

    fix sg's and subnets

--- eks.tf
+++ eks.tf
@@ -10,7 +10,7 @@ locals {
 }
 
 module "security_groups" {
-  source  = "git::https://github.com/drewpypro/terraform-aws-sg-module-template.git?ref=v2.0.9"
+  source  = "git::https://github.com/drewpypro/terraform-aws-sg-module-template.git?ref=v2.0.10"
 
   vpc_id = module.vpc.vpc_id
 
