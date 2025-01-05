c7c9c7e881a8382bcb978e936869d434e6a1cc9a
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-01-05 11:17:23 -0700

    add ecr in vars

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -96,7 +96,7 @@ variable "services" {
   default = ["autoscaling", "dms", "ec2", "ec2messages",
     "elasticloadbalancing", "logs", "monitoring", "rds",
     "kms", "sns", "sqs", "ssm",
-  "ssmmessages", "sts", ]
+  "ssmmessages", "sts", "ecr" ]
 }
 
 variable "gateway_services" {
