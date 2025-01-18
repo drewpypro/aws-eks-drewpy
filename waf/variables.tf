e7451ae48576e6d02de91f8f59f4f46c537bacc8
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-01-18 14:31:08 -0700

    updated waf

--- waf/variables.tf
+++ waf/variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "CLOUDFLARE_API_TOKEN" {
   description = "cloudflare api token"
   type = string
